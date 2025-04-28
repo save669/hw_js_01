fb2139b70362df51b84e9d343a6a996af793e270
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-04-28 18:10:40 +0300

    doing hw01-01

---
+++ lesson-02/01.js
@@ -0,0 +1,28 @@
+/*
+* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+* Полученное булево значение сохраните в переменной isAccess.
+
+* Условия доступа:
+* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
+* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+*/
+
+// тестовые данные (значения можно менять)
+const isAdmin = false
+const isVerifiedUser = false
+const hasSpecialPermission = false
+const hasTemporaryPass = false
+
+let isAccess
+
+// your code
+
+if ((isAdmin || isVerifiedUser)) {
+    if(hasSpecialPermission || hasTemporaryPass)
+    isAccess = true
+} else {
+    isAccess =false
+}
+
+console.log(isAccess);
+
