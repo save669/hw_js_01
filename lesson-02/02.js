946e36ce4ad4907eaae6f3025188d9de59924be4
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-05-08 00:33:01 +0300

    vgerge

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -17,10 +17,10 @@ while(!isWinningDouble) {
   console.log('Первый бросок: ' + dice1)
   console.log('Второй бросок: ' + dice2)
   if(dice1 === dice2 && dice1 > 3 && dice2 > 3) {
-    alert("Win!")
+    console.log('Выигрышный дубль!')
     isWinningDouble = true
   } else {
-    alert('loose')
+    console.log('Не выигрышный дубль.')
     
   }
 }
@@ -29,7 +29,7 @@ while(!isWinningDouble) {
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+  
 } else {
   console.log('Не выигрышный дубль.')
 }
