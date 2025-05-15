b451e634834ff23bd4aded724503d0f162d9cfdc
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-05-15 20:41:59 +0300

    hw3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(num1, num2) {
+    const result = (num1 + num2) / 2
+    return result
+}
+
+
