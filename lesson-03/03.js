008243d1bee9ed0544d3775357757ef0b623a99c
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-05-15 20:51:46 +0300

    hw3-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+    const result = Math.max(num1, num2, num3)
+    return result
+}
+
