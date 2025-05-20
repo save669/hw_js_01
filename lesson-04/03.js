228ea2af40d74bf4d042cce9ddb3f4407f500153
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-05-20 05:08:19 +0300

    Update 03.js

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -20,7 +20,7 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 function findCommonElements(array1, array2) {
     const commonArray = []
     for (i = 0; i < length.array1; i++){
-        if (array2.includes(array1[i]))
+        if (array2.includes(array1[i]) && !commonElements.includes(array1[i]))
             commonArray.push(array1[i])
     }
     return commonArray
